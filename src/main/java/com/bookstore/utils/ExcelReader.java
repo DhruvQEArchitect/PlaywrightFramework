eb66330292bdc019b612dfe4b0c36daaff62ecf7
dhruv.kmr.2101@gmail.com <dhruv.kmr.2101@gmail.com>
2025-01-18 15:22:30 -0500

    adding support for excel reading for test cases

--- src/main/java/com/bookstore/utils/ExcelReader.java
+++ src/main/java/com/bookstore/utils/ExcelReader.java
@@ -1,4 +1,4 @@
-package utils;
+package com.bookstore.utils;
 
 import com.bookstore.factory.Factory;
 import org.apache.poi.ss.usermodel.CellType;
@@ -7,7 +7,6 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -63,7 +62,7 @@ public class ExcelReader {
         return colCount;
     }
 
-    public static List<Object> getCellData(String cellName) {
+    public static List<Object> getEntireExcelData() {
         List<Object> cellData = new LinkedList<>();
         for (int i = 1; i <= getRowCount(); i++) {
             for (int j = 0; j < getColCount(); j++) {
@@ -98,8 +97,46 @@ public class ExcelReader {
         return cellData;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static List<Object> getColumnData(String columnName) {
+        List<Object> cellData = new LinkedList<>();
+        for (int i = 1; i <= getRowCount(); i++) {
+            xssfRow = xssfSheet.getRow(i);
+            for (int j = 0; j < getColCount(); j++) {
+                xssfCell = xssfRow.getCell(j);
+                if (xssfSheet.getRow(0).getCell(j).getStringCellValue().equalsIgnoreCase(columnName)) {
+
+                    CellType cellType = xssfCell.getCellType();
+                    switch (cellType) {
+                        case STRING -> {
+                            cellData.add(xssfCell.getStringCellValue());
+                            break;
+                        }
+                        case NUMERIC -> {
+                            cellData.add(xssfCell.getNumericCellValue());
+                            break;
+                        }
+                        case BOOLEAN -> {
+                            cellData.add(xssfCell.getBooleanCellValue());
+                            break;
+                        }
+                        case FORMULA -> {
+                            cellData.add(xssfCell.getCellFormula());
+                            break;
+                        }
+                        default -> {
+                            System.out.println("unable to read value");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return cellData;
+    }
+
+    public static void main(String[] args) {
         ExcelReader excelReader = new ExcelReader();
-        System.out.println(getCellData("UserName"));
+        System.out.println(getEntireExcelData());
+        System.out.println(getColumnData("UserName"));
     }
 }
